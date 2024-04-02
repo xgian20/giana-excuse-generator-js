22532bfe50f31bdb49783969ee00b0e7d8e1d635
xgian20 <162388975+xgian20@users.noreply.github.com>
2024-04-02 00:52:18 +0000

    finished excuse generator project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Create the "parts of the sentence" arrays
+  let who = ["The dog", "My grandma", "The mailman", "The bird"];
+  let action = ["ate", "threw away", "mailed", "stole"];
+  let what = ["my homework", "my cellphone", "the car"];
+  let when = ["yesterday!", "this morning!", "last week!"];
+
+  //combine random elements in each array to form a sentence
+  // create function to select element in array
+  function selectArrayElement(arrayName, arrayLength) {
+    //generate a random number based on length of arrayName
+    let randomNumber = Math.floor(Math.random() * arrayLength);
+
+    //asign an element from arrayName to be returned
+    let selectedElement = arrayName[randomNumber];
+    return selectedElement;
+  }
+
+  let randomWho = selectArrayElement(who, who.length);
+  let randomAction = selectArrayElement(action, action.length);
+  let randomWhat = selectArrayElement(what, what.length);
+  let randomWhen = selectArrayElement(when, when.length);
+
+  //string templates
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  //retrieving html elements to manipulate
+  const htmlElement = document.querySelector(".excuse");
+  htmlElement.innerHTML = excuse;
 };
